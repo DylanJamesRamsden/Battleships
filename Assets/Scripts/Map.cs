94f7a232d563418ae07d6e1acba04b962a27a17e
DylanJamesRamsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2018-10-03 14:25:02 +0200

    Methods and dsiplaying of map added

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,21 +5,62 @@ using UnityEngine;
 public class Map : MonoBehaviour {
 
     public GameObject oceanTile;
-    public float size;
+    public GameObject missTile;
+    public GameObject hitTile;
+    public int size;
+    private Tile[,] map;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        createMap();
+        dsiplayMap();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		for (int x = 0;  x < size; x++)
+		
+	}
+
+    void createMap()
+    {
+        map = new Tile[size, size];
+        for (int x = 0; x < size; x++)
         {
             for (int z = 0; z < size; z++)
             {
-                Instantiate(oceanTile, new Vector3(x, 0, z), Quaternion.identity);
+                Tile t = new Tile();
+                if (Random.Range(0, 100) > 60)
+                {
+                    t.typeOfTile = tileType.Hit;
+                }
+                else
+                {
+                    t.typeOfTile = tileType.Ocean;
+                }
+                map[x, z] = t;
             }
         }
-	}
+    }
+
+    void dsiplayMap()
+    {
+        for (int x = 0; x < size; x++)
+        {
+            for (int z = 0; z < size; z++)
+            {
+                switch (map[x, z].typeOfTile)
+                {
+                    case tileType.Ocean:
+                        Instantiate(oceanTile, new Vector3(x, 0, z), Quaternion.identity);
+                        break;
+                    case tileType.Miss:
+                        Instantiate(missTile, new Vector3(x, 0, z), Quaternion.identity);
+                        break;
+                    case tileType.Hit:
+                        Instantiate(hitTile, new Vector3(x, 0, z), Quaternion.identity);
+                        break;
+                }
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+
+    public enum tileType { Ocean, Hit, Miss};
+
+    class Tile
+    {
+        public tileType typeOfTile { get; set; }
+
+
+
+
+
+
+    }
